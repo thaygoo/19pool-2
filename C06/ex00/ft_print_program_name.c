1c5b2eba74d4b46e1cd972afd8b2d6202da7a4fd
thaygoo <hugo.burton6@gmail.com>
2024-09-17 14:29:17 +0200

    0

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,27 @@
 /*   By: huburton <huburton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 14:27:15 by huburton          #+#    #+#             */
-/*   Updated: 2024/09/17 14:27:28 by huburton         ###   ########.fr       */
+/*   Updated: 2024/09/17 14:29:13 by huburton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		write(1, &str[i], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc)
+	{
+		ft_putstr(argv[0]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
