d79fe6b0ba77907e6c5b0790ea4bba6b467214e7
thaygoo* <hugogogo6@gmaiL.com>
2024-09-18 16:37:01 +0200

    0

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -10,3 +10,74 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*ret;
+	int		i;
+
+	i = 0;
+	ret = malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (!ret)
+		return (NULL);
+	while (src[i])
+	{
+		ret[i] = src[i];
+		i++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+void	free_copy(t_stock_str *tab, int i)
+{
+	int	a;
+
+	a = 0;
+	while (a <= i)
+	{
+		free(tab[i].copy);
+		a++;
+	}
+	free(tab);
+	return ;
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*tab;
+
+	if (ac < 1)
+		ac = 0;
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(tab[i].str);
+		if (!tab[i].copy)
+		{
+			free_copy(tab, i);
+			return (NULL);
+		}
+		i++;
+	}
+	tab[i].str = 0;
+	return (tab);
+}
